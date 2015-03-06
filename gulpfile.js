b7ab80645c5888ad4a9a317ea2bdbfcdb665e75e
Wyatt Johnson <wyattjoh@gmail.com>
2015-03-06 15:20:00 -0700

    New style

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 var gulp = require('gulp');
 var fs = require("fs");
 var s3 = require("gulp-s3");
+var revall = require('gulp-rev-all');
+
 var awsCredentials = JSON.parse(fs.readFileSync('./aws.json'));
 
-gulp.task('upload', function() {
+var jsyaml = require('js-yaml');
+var jekylConfig = jsyaml.load(fs.readFileSync('./_config.yml'));
+
+gulp.task('manifest', function() {
   gulp.src('./assets/**')
-      .pipe(s3(awsCredentials, {
-        uploadPath: "/assets/",
-        headers: {
-          'x-amz-acl': 'public-read'
-        }
-      }));
+  .pipe(revall())
+  .pipe(revall.manifest({ fileName: 'manifest.json', prefix: jekylConfig["asset_url"] }))
+  .pipe(gulp.dest('./_data'));
+})
+
+gulp.task('upload', ['manifest'], function() {
+  gulp.src('./assets/**')
+    .pipe(revall())
+    .pipe(s3(awsCredentials, {
+      uploadPath: "/assets/",
+      headers: {
+        'x-amz-acl': 'public-read'
+      }
+    }));
 });
